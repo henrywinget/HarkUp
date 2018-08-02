159ce792c8563ebe2a480f60c1dab3754393f1e9
henrywinget <henrywinget@gmail.com>
2018-08-01 23:22:20 -0500

    first commit

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,8 @@ const mysql = require("mysql");
 var connection = "";
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    // connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = "mysql://a8cvv8e793ilfmxb:vurbjc4p9c4smxew@cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/pb3kycf2wo314bl0";
 } else {
  connection = mysql.createConnection({
     host: "localhost",
