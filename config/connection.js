373b6d0046d5b9f438596504ff7607ecfdf3fcc0
henrywinget <henrywinget@gmail.com>
2018-08-01 23:00:41 -0500

    updates

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,16 @@ const mysql = require("mysql");
 // // Configure MySQL connection
 var connection = "";
 
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
  connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "HLWxs20",
     database: "HarkUp"
     });
-// }
+}
 // // Establish MySQL connection    
 connection.connect(function (err) {
     if (err) throw err;
