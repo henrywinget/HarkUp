ef0602128edacae581b338d2e916709c2e4adddb
henrywinget <henrywinget@gmail.com>
2018-08-01 23:24:43 -0500

    first commit

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection = "";
 
 if (process.env.JAWSDB_URL) {
     // connection = mysql.createConnection(process.env.JAWSDB_URL);
-    connection = "mysql://a8cvv8e793ilfmxb:vurbjc4p9c4smxew@cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/pb3kycf2wo314bl0";
+    connection = mysql.createConnection("mysql://a8cvv8e793ilfmxb:vurbjc4p9c4smxew@cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/pb3kycf2wo314bl0");
 } else {
  connection = mysql.createConnection({
     host: "localhost",
